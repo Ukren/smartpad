e70bc864a659da538d3330514224d2335be9a93e
Artem Zaharchuk <artem.zaharchuk1@gmail.com>
2026-04-21 17:17:27 +0300

    Modify Header component

--- frontend/src/app/layout/Header/Header.tsx
+++ frontend/src/app/layout/Header/Header.tsx
@@ -1,22 +1,26 @@
-import { AppBar, Toolbar, Typography } from '@mui/material'
+import { AppBar, Box, Toolbar, Typography } from '@mui/material'
+import { SIDEBAR_WIDTH } from '../const/sidebar'
 
 import { ThemeToggle } from './components/ThemeToggle'
+import { SearchInput } from './components/SearchInput'
+import { UserMenu } from './components/UserMenu'
 
-interface HeaderProps {
-  sidebarWidth: number
-}
-
-export const Header = ({ sidebarWidth }: HeaderProps) => (
+export const Header = () => (
   <AppBar
     position="static"
     elevation={1}
     sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}
   >
     <Toolbar>
-      <Typography variant="h6" sx={{ width: sidebarWidth, flexShrink: 0 }}>
+      <Typography variant="h6" sx={{ width: SIDEBAR_WIDTH, flexShrink: 0 }}>
         Smartpad
       </Typography>
+      <SearchInput />
+
+      <Box sx={{ flex: 1 }} />
+
       <ThemeToggle />
+      <UserMenu />
     </Toolbar>
   </AppBar>
 )
