f54d78fec3863dc3d4ef8c6cced1723adbc67ee4
Artem Zaharchuk <artem.zaharchuk1@gmail.com>
2026-05-04 12:03:35 +0300

    Initialize NestJS app

---
+++ backend/src/main.ts
@@ -0,0 +1,15 @@
+import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import cookieParser from 'cookie-parser';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+  app.use(cookieParser());
+  app.enableCors({ origin: 'http://localhost:5173', credentials: true });
+
+  await app.listen(process.env.PORT ?? 3000);
+}
+void bootstrap();
