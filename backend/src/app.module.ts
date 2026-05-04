c476e7b9acafe19315dae61e1ee8d41947072818
Artem Zaharchuk <artem.zaharchuk1@gmail.com>
2026-05-04 14:22:56 +0300

    Configure Docker and Prisma

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,6 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
+import { NotesModule } from './notes/notes.module';
+import { TagsModule } from './tags/tags.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    PrismaModule,
+    AuthModule,
+    NotesModule,
+    TagsModule,
+  ],
 })
 export class AppModule {}
