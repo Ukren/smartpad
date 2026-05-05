5ed500a51de99795d7b2492539307ed2af635dea
Artem Zaharchuk <artem.zaharchuk1@gmail.com>
2026-05-05 20:15:28 +0300

    Setup TanStack QueryClientProvider in main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { AppRouter } from './app/AppRouter'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <AppRouter />
+    <QueryClientProvider client={new QueryClient()}>
+      <AppRouter />
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   </React.StrictMode>
 )
