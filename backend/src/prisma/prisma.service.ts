b64c1c9f1e664ba53fbe39a421699d332a66193d
Artem Zaharchuk <artem.zaharchuk1@gmail.com>
2026-05-18 12:03:44 +0300

    Move pg.Pool creation into constructor so dotenv loads first

--- backend/src/prisma/prisma.service.ts
+++ backend/src/prisma/prisma.service.ts
@@ -1,14 +1,14 @@
+import 'dotenv/config'
 import { Injectable, OnModuleInit } from '@nestjs/common'
 import { PrismaClient } from '@prisma/client'
 import { PrismaPg } from '@prisma/adapter-pg'
 import pg from 'pg'
 
-const pool = new pg.Pool({ connectionString: process.env.DATABASE_URL })
-const adapter = new PrismaPg(pool)
-
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
   constructor() {
+    const pool = new pg.Pool({ connectionString: process.env.DATABASE_URL })
+    const adapter = new PrismaPg(pool)
     super({ adapter })
   }
 
